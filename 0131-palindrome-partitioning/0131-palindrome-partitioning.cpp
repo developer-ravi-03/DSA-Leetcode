cc38322d030faa90cff7c73bef0ff76287f63bd1
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-01 17:21:05 +0530

    Time: 48 ms (52.26%), Space: 57.4 MB (58.66%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    bool isPallindrome(string part){
-        string cp=part;
-        reverse(cp.begin(),cp.end());
-        if(cp==part)return true;
-        return false;
+    bool isPallindrome(string &s) {
+        int l=0,r=s.size()-1;
+        while (l < r) {
+            if (s[l] != s[r])
+                return false;
 
+            l++;
+            r--;
+        }
+
+        return true;
     }
-    void solve(int index,vector<string> &partition,vector<vector<string>> &ans,string s){
-        if(index==s.size()){
+    void solve(int index, vector<string>& partition,
+               vector<vector<string>>& ans, string s) {
+        if (index == s.size()) {
             ans.push_back(partition);
             return;
         }
 
-        for(int i=index;i<s.size();i++){
-            string part=s.substr(index,i-index+1);
-            if(isPallindrome(part)){
+        for (int i = index; i < s.size(); i++) {
+            string part = s.substr(index, i - index + 1);
+            if (isPallindrome(part)) {
                 partition.push_back(part);
-                solve(i+1,partition,ans,s);
+                solve(i + 1, partition, ans, s);
                 partition.pop_back();
             }
         }
@@ -25,7 +31,7 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> partition;
-        solve(0,partition,ans,s);
+        solve(0, partition, ans, s);
         return ans;
     }
 };
