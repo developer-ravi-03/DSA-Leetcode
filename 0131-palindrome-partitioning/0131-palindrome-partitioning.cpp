b98d875abd316f544d24e01a4522fca536defcde
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-01 17:15:09 +0530

    Time: 58 ms (38.86%), Space: 58.8 MB (49.61%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPallindrome(string part){
+        string cp=part;
+        reverse(cp.begin(),cp.end());
+        if(cp==part)return true;
+        return false;
+
+    }
+    void solve(int index,vector<string> &partition,vector<vector<string>> &ans,string s){
+        if(index==s.size()){
+            ans.push_back(partition);
+            return;
+        }
+
+        for(int i=index;i<s.size();i++){
+            string part=s.substr(index,i-index+1);
+            if(isPallindrome(part)){
+                partition.push_back(part);
+                solve(i+1,partition,ans,s);
+                partition.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> partition;
+        solve(0,partition,ans,s);
+        return ans;
+    }
+};
