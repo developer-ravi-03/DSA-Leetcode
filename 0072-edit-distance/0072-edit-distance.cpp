670ca8094a0b21553b29b81df78fb7ae1f7c53b6
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-17 20:44:42 +0530

    Time: 2 ms (93.69%), Space: 10.5 MB (93.77%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,46 +1,26 @@
 class Solution {
-public:
-    int helper(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
-        // exhausted
-        if (i == 0)
-            return j;
-        if (j == 0)
-            return i;
-
-        // check dp for result
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        // if matching
-        if (s1[i - 1] == s2[j - 1]) {
-            return dp[i][j] = 0 + helper(i - 1, j - 1, s1, s2, dp);
-        }
-        // try insert, delete, and replace
-        return dp[i][j] = 1 + min(helper(i, j - 1, s1, s2, dp),
-                                  min(helper(i - 1, j, s1, s2, dp),
-                                      helper(i - 1, j - 1, s1, s2, dp)));
-    }
+public:    
     int minDistance(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        // memoization
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // Tabulation and space optimisation
+        vector<int> prev(m + 1,0),cur(m + 1,0);
 
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = i; // j==0 return i
         for (int j = 0; j <= m; j++)
-            dp[0][j] = j; // i==0 return j
+            prev[j] = j; // i==0 return j
 
         for (int i = 1; i <= n; i++) {
+            cur[0]=i;
             for (int j = 1; j <= m; j++) {
                 if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = 0 + helper(i - 1, j - 1, s1, s2, dp);
+                    cur[j] = prev[j-1];
                 } else{
                     // try insert, delete, and replace
-                    dp[i][j] = 1 + min(dp[i][j - 1],min(dp[i - 1][j], dp[i - 1][j - 1]));
+                    cur[j] = 1 + min(prev[j],min(cur[j-1], prev[j-1]));
                 }
             }
+            prev=cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
