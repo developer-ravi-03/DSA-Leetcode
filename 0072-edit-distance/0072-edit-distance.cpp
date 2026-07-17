e69a816db2d078ec2550174238f22640e5709fe4
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-17 20:07:34 +0530

    Time: 4 ms (81.3%), Space: 12.8 MB (81.99%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int helper(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+        //exhausted
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+
+        //check dp for result
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        //if matching 
+        if(s1[i]==s2[j]){
+            return dp[i][j]=0+helper(i-1,j-1,s1,s2,dp);
+        }
+        //try insert, delete, and replace
+        return dp[i][j]=1+min(helper(i,j-1,s1,s2,dp),min(helper(i-1,j,s1,s2,dp),helper(i-1,j-1,s1,s2,dp)));
+    }
+    int minDistance(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        //memoization
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return helper(n-1,m-1,s1,s2,dp);
+    }
+};
