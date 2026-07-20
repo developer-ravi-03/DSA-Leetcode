a871ba5852e8a83eb0f501cb747668a5399dbcf3
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-20 15:06:57 +0530

    Time: 279 ms (32.32%), Space: 296.4 MB (27.3%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int solve(int idx, int prevIdx, int n, vector<int>& nums,
-              vector<vector<int>>& dp) {
-        if (idx == n)
-            return 0;
-
-        if (dp[idx][prevIdx + 1] != -1)
-            return dp[idx][prevIdx + 1];
-
-        int skip = solve(idx + 1, prevIdx, n, nums, dp);
-        int take = 0;
-        if (prevIdx == -1 || nums[prevIdx] < nums[idx])
-            take = 1 + solve(idx + 1, idx, n, nums, dp);
-
-        return dp[idx][prevIdx + 1] = max(take, skip);
-    }
+    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        int prevIdx = -1;
-        return solve(0, prevIdx, n, nums, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prevIdx = idx - 1; prevIdx >= -1; prevIdx--) {
+
+                int skip = dp[idx + 1][prevIdx+1];
+                int take = 0;
+                if (prevIdx == -1 || nums[prevIdx] < nums[idx])
+                    take = 1 + dp[idx + 1][idx+1];
+
+                dp[idx][prevIdx + 1] = max(take, skip);
+            }
+        }
+
+        return dp[0][0];
     }
 };
