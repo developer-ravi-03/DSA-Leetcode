00a53e56f429778b8d3cdf23746c637c290e5a7a
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-22 07:55:58 +0530

    Time: 20 ms (84.78%), Space: 27.3 MB (78.5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int helper(int i,int j,int n,int m,string &t1,string &t2,vector<vector<int>>&dp){
-        if(i==n || j==m) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(t1[i]==t2[j]) return dp[i][j]=1+helper(i+1,j+1,n,m,t1,t2,dp);
-        int skip1=helper(i+1,j,n,m,t1,t2,dp);
-        int skip2=helper(i,j+1,n,m,t1,t2,dp);
-        return dp[i][j]=max(skip1,skip2);
-    }
-
+    
     int longestCommonSubsequence(string t1, string t2) {
-        int n=t1.size();
-        int m=t2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(0,0,n,m,t1,t2,dp);
+        int n = t1.size();
+        int m = t2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        //Tabulation method bottom right direction
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (t1[i] == t2[j])
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else {
+                    int skip1 = dp[i + 1][j];
+                    int skip2 = dp[i][j + 1];
+                    dp[i][j] = max(skip1, skip2);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
