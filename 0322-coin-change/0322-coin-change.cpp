877daf37f82a53c40a8c19a6b33ab615ff34a0de
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-23 14:47:03 +0530

    Time: 55 ms (18.53%), Space: 49.7 MB (20.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
-
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i - coin >= 0) {
-                    dp[i] = min(dp[i], 1 + dp[i - coin]);
-                }
-            }
+  
+    int helper(int i, int n, int amount, vector<int>& coins,vector<vector<int>> &dp) {
+        if (amount == 0) {            
+            return 0;
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        if (i == n)
+            return 1e9;
+
+        if(dp[i][amount]!=-1) return dp[i][amount];
+
+        int yes=1e9;
+        if (coins[i] <= amount) {
+            yes=1+helper(i, n, amount - coins[i], coins,dp);
+        }
+        int no=helper(i + 1, n, amount, coins,dp);
+        return dp[i][amount]=min(yes,no);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans=helper(0, n, amount, coins,dp);
+        return ans>=1e9?-1:ans;
     }
 };
