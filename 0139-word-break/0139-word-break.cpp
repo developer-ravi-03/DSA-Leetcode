0328e50dd641f7213c35f2e78e22b20348a88ba0
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-07-25 12:39:13 +0530

    Time: 0 ms (100%), Space: 10.6 MB (86.34%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    bool helper(int i, string &s, vector<string>& wordDict,vector<int>&dp) {
-        if (i == s.size())
-            return true;
-        
-        if(dp[i]!=-1) return dp[i];
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> dp(s.size() + 1, false);
+        dp[s.size()] = true;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (auto& word : wordDict) {
+                if (s.substr(i, word.size()) == word) {
+                    if (dp[i + word.size()]) {
 
-        for (auto& word : wordDict) {
-            if (s.substr(i, word.size()) == word) {
-                if (helper(i + word.size(), s, wordDict,dp))
-                    return dp[i]=true;
+                        dp[i] = true;
+                        break;
+                    }
+                }
             }
         }
-        
-        return dp[i]=false;
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-       vector<int>dp(s.size()+1,-1);
-        return helper(0, s, wordDict,dp);
+        return dp[0];
     }
 };
