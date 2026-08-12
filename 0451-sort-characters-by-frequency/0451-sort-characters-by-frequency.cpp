fe0b8e806feaafe8ef5a5d05a25ed39c81604a67
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-08-12 23:03:37 +0530

    Time: 6 ms (24.7%), Space: 10.9 MB (80.26%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        for(char ch:s){
+            mp[ch]++;
+        }
+
+        priority_queue<pair<int,char>>pq;
+        for(auto &it:mp){
+            pq.push({it.second,it.first});
+        }
+
+        string ans="";
+
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+
+            while(it.first--){
+                ans+=it.second;
+            }
+        }
+        return ans;
+    }
+};
