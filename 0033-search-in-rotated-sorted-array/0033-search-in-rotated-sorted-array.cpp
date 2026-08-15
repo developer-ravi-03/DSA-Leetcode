a37fc0754072dead61ce176736637a7fab66f975
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-08-15 23:44:21 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -7,17 +7,17 @@ public:
             int mid=low+(high-low)/2;
 
             if(nums[mid]==target) return mid;
-            if(nums[mid]>=nums[high]){//left sorted
-                if(nums[low]>target){
-                    low=mid+1;
-                }else{
+            if(nums[low]<=nums[mid]){//left sorted
+                if(nums[low]<=target && target<nums[mid]){
                     high=mid-1;
+                }else{
+                    low=mid+1;
                 }
             }else{
-                if(nums[mid]>target){
-                    high=mid-1;
-                }else{
+                if(nums[mid]<target && target<=nums[high]){
                     low=mid+1;
+                }else{
+                    high=mid-1;
                 }
             }
         }
