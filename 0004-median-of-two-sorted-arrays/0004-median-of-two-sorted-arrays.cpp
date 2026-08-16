be5d9814aa41f685387a463a1c1205fa4ced511f
Ravi Kumar <115774444+developer-ravi-03@users.noreply.github.com>
2026-08-16 18:35:48 +0530

    Time: 0 ms (100%), Space: 95.1 MB (61.66%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,19 +1,52 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size(),n=nums2.size();
-        vector<int>tmp;
-        for(int n:nums1) tmp.push_back(n);
-        for(int n:nums2) tmp.push_back(n);
-        sort(tmp.begin(),tmp.end());
+        int m = nums1.size(), n = nums2.size();
+        int total = m + n;
+        if (m > n)
+            return findMedianSortedArrays(nums2, nums1);
+        // if (m > n)
+        //      swap(nums1, nums2);
 
-        double ans;
-        int mid=0+(m+n)/2;
-        if((m+n)%2==1){
-            ans=tmp[mid];
-        }else{
-            ans=(tmp[mid]+tmp[mid-1])/2.0;
+        int low = 0, high = m;
+        // int leftSize = (m + n + 1) / 2;
+
+        while (low <= high) {
+            // Partition of nums1
+            int partitionA = (low + high) / 2;
+
+            // Partition of nums2
+            int partitionB = (m + n + 1) / 2 - partitionA;
+
+            int leftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+
+            int rightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+
+            int leftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+
+            int rightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+
+            if (leftA <= rightB && leftB <= rightA) {
+
+                // Odd total elements
+                if ((m + n) % 2 == 1) {
+                    return max(leftA, leftB);
+                }
+
+                // Even total elements
+                return (max(leftA, leftB) + min(rightA, rightB)) / 2.0;
+            }
+
+            // Too many elements taken from nums1
+            else if (leftA > rightB) {
+                high = partitionA - 1;
+            }
+
+            // Too few elements taken from nums1
+            else {
+                low = partitionA + 1;
+            }
         }
-        return ans;
+        return 0.0;
     }
 };
